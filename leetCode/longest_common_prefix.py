ec2a8f1fbdabfbd1d72f376996ab49a902564b4f
michalorlinski <michal.orlinski@relayr.io>
2023-08-26 20:52:23 +0200

    feat: complex validations

--- leetCode/longest_common_prefix.py
+++ leetCode/longest_common_prefix.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/maximum-length-of-pair-chain/
+https://leetcode.com/problems/longest-common-prefix/
 
 Maximum Length of Pair Chain
     You are given an array of n pairs pairs where
@@ -8,18 +8,36 @@ Maximum Length of Pair Chain
     A chain of pairs can be formed in this fashion.
     Return the length longest chain which can be formed.
 
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
 Example:
-  Input: pairs = [[1,2],[2,3],[3,4]]
- Output: 2
+  Input: strs = ["flower","flow","flight"]
+ Output: "fl"
 """
 
 from typing import List
+import re
 
 
 class Solution:
 
-    def _check_valid(self, strs: List[str]) -> str:
-        assert strs, f"{strs}"
-        assert 1 <= len(strs) <= 200, f"{num} has invalid value: from 1 to 3999 !!!\n"
+    def __check_valid(self, strs: List[str]) -> str:
+        # check if input is a list with length from 1 to 200
+        assert type(strs) == list, f"{strs} is NOT a list !!!\n"
+        assert 1 <= len(strs) <= 200, f"{len(strs)} has invalid value: from 1 to 200 !!!\n"
+        
+        # check if each element of input list has length from 0 to 200
+        for i_str in strs:
+            assert 0 <= len(i_str) <= 200, f"{i_str} length has invalid value: \
+                                            0 <= single_string <= 200 !!!\n"
+
+        # check if all strings consists of only lowercase English letters 
+        eng_lowercase = re.compile("[a-z]+")
+        assert all(eng_lowercase.fullmatch(i_str) for i_str in strs if i_str), \
+            f"{strs} list should consists of only lowercase English letters"
+        
 
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        self.__check_valid(strs)
+        
